1641ffbdc7845d81141cb9c07db5856b84b2e3b6
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-08-11 23:01:04 +0300

    feat: add input creator in registration-view.ts

--- src/components/view/page/registration-view/registration-view.ts
+++ src/components/view/page/registration-view/registration-view.ts
@@ -1,21 +1,38 @@
+import styles from './registration-view.module.scss';
 import TagName from '../../../../enum/tag-name';
 import { ElementParams } from '../../../../utils/element-creator';
 import DefaultView from '../../default-view';
-import styleCss from './registration-view.module.scss';
+import InputCreator from '../../../../utils/input/inputCreator';
 
 export default class RegistrationView extends DefaultView {
   constructor() {
     const params: ElementParams = {
       tag: TagName.SECTION,
-      classNames: Object.values(styleCss),
+      classNames: Object.values(styles),
       textContent: 'RegistrationView',
     };
     super(params);
 
-    // this.configView();
+    this.configView();
   }
 
   private configView() {
-    throw new Error(`configView() for ${this.getElement()} not implemented`);
+    const emailInput = new InputCreator({
+      classNames: [styles.registrationView__form],
+      textContent: 'email',
+      callback: this.changeAny,
+      attributes: {
+        type: 'email',
+        name: 'email',
+        placeholder: 'print email',
+        Required: true,
+      },
+    });
+
+    this.getElement().append(emailInput.getElement());
+  }
+
+  private changeAny(e: Event): void {
+    alert(e);
   }
 }
