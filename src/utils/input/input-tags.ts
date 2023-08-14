be2d357857cb7d82e278ce12a8743f6fa270e712
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-08-14 15:55:10 +0300

    refactor: delete an unnecessary file

--- src/utils/input/input-tags.ts
+++
@@ -1,7 +0,0 @@
-// import ElementCreator from '../element-creator';
-// import TagName from '../../enum/tag-name';
-//
-// export default class inputFieldCreator extends ElementCreator {
-//
-//   }
-// }
