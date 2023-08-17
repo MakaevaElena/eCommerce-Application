ff63886e31fdea2cfe42825f8b7d8991ca007c46
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-08-17 23:16:23 +0300

    feat: add postal-paterns and postal-tittles

---
+++ src/components/view/page/registration-view/inputs-params/postal-titles.ts
@@ -0,0 +1,11 @@
+enum PostalTitles {
+  SIX_DIGITS = 'Must consist of six numbers',
+  FIVE_DIGITS = 'Must consist of five numbers',
+  FOUR_DIGITS = 'Must consist of four numbers',
+  TWO_DIGITS = 'Must consist of two numbers',
+  TWO_LETTERS_THREE_DIGITS = 'Must consist of two capital letters and three numbers',
+  TWO_LETTERS_FOUR_DIGITS = 'Must consist of two capital letters and four numbers',
+  TWO_LETTERS_FIVE_DIGITS = 'Must consist of two capital letters and five numbers',
+}
+
+export default PostalTitles;
