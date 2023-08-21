43b2f55bcfbd07e38466a13419394291681d7db6
VladimirSobbolev <vladimir@sobbolev.ru>
2023-08-21 15:36:49 +0300

    refactor: add enums with status code and delays

--- src/components/view/page/registration-view/registration-view.ts
+++ src/components/view/page/registration-view/registration-view.ts
@@ -27,6 +27,9 @@ import { PagePath } from '../../../router/pages';
 import ClientApi from '../../../../api/client-api';
 import EventName from '../../../../enum/event-name';
 import Observer from '../../../../observer/observer';
+import delays from '../../../../enum/delays';
+import Delays from '../../../../enum/delays';
+import StatusCodes from '../../../../enum/status-codes';
 
 export default class RegistrationView extends DefaultView {
   private readonly DEFAULT_ADDRESS_NUMBER = 1;
@@ -558,10 +561,10 @@ export default class RegistrationView extends DefaultView {
       api
         .createCustomer(params)
         .then((response) => {
-          if (response.statusCode === 201) {
+          if (response.statusCode === StatusCodes.USER_CREATED) {
             this.checkValidityElement.textContent = TextContents.REGISTRATION_OK;
             this.getElement().append(this.checkValidityElement);
-            setTimeout(() => this.router.navigate(PagePath.LOGIN), 2000);
+            setTimeout(() => this.router.navigate(PagePath.LOGIN), Delays.SWITCH_TO_PAGE);
           }
         })
         .then(() => {
