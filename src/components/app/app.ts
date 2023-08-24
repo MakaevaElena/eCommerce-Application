7174b46bbc412e89b55f2fb3ec00603a2ac23eb2
surface74 <warwhisper@tut.by>
2023-08-24 17:16:49 +0300

    refactor: realize direct import & call <Page>View without separate call getView()

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -52,10 +52,10 @@ export default class App {
       {
         path: ``,
         callback: async () => {
-          const { getView } = await import('../view/page/index-view/index-view');
+          const IndexView = (await import('../view/page/index-view/index-view')).default;
           const view: DefaultView | undefined = this.viewStorage.has(PagePath.INDEX)
             ? this.viewStorage.get(PagePath.INDEX)
-            : getView(this.router);
+            : new IndexView(this.router);
           if (view) {
             this.viewStorage.set(PagePath.INDEX, view);
             this.setContent(PagePath.INDEX, view);
@@ -65,10 +65,10 @@ export default class App {
       {
         path: `${PagePath.INDEX}`,
         callback: async () => {
-          const { getView } = await import('../view/page/index-view/index-view');
+          const IndexView = (await import('../view/page/index-view/index-view')).default;
           const view: DefaultView | undefined = this.viewStorage.has(PagePath.INDEX)
             ? this.viewStorage.get(PagePath.INDEX)
-            : getView(this.router);
+            : new IndexView(this.router);
           if (view) {
             this.viewStorage.set(PagePath.INDEX, view);
             this.setContent(PagePath.INDEX, view);
@@ -78,10 +78,10 @@ export default class App {
       {
         path: `${PagePath.LOGIN}`,
         callback: async () => {
-          const { getView } = await import('../view/page/login-view/login-view');
+          const LoginView = (await import('../view/page/login-view/login-view')).default;
           const view: DefaultView | undefined = this.viewStorage.has(PagePath.LOGIN)
             ? this.viewStorage.get(PagePath.LOGIN)
-            : getView(this.router);
+            : new LoginView(this.router);
           if (localStorage.getItem('isLogin') === 'true') {
             this.router.navigate(PagePath.INDEX);
           }
@@ -94,10 +94,10 @@ export default class App {
       {
         path: `${PagePath.REGISTRATION}`,
         callback: async () => {
-          const { getView } = await import('../view/page/registration-view/registration-view');
+          const RegistrationView = (await import('../view/page/registration-view/registration-view')).default;
           const view: DefaultView | undefined = this.viewStorage.has(PagePath.REGISTRATION)
             ? this.viewStorage.get(PagePath.REGISTRATION)
-            : getView(this.router);
+            : new RegistrationView(this.router);
           if (view) {
             this.viewStorage.set(PagePath.REGISTRATION, view);
             this.setContent(PagePath.REGISTRATION, view);
@@ -107,10 +107,10 @@ export default class App {
       {
         path: `${PagePath.NOT_FOUND}`,
         callback: async () => {
-          const { getView } = await import('../view/page/not-found-view/not-found-view');
+          const NotFoundView = (await import('../view/page/not-found-view/not-found-view')).default;
           const view: DefaultView | undefined = this.viewStorage.has(PagePath.NOT_FOUND)
             ? this.viewStorage.get(PagePath.NOT_FOUND)
-            : getView(this.router);
+            : new NotFoundView(this.router);
           if (view) {
             this.viewStorage.set(PagePath.NOT_FOUND, view);
             this.setContent(PagePath.NOT_FOUND, view);
