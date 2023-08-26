8fdb9e9b8088ddef1e8ba8db47d7109cdc17c260
Makaeva Elena <87429007+MakaevaElena@users.noreply.github.com>
2023-08-26 21:47:56 +0700

    fix: if price is missing (#113)
    * feat: add product page
    
    * fix: merge product_page with routing
    
    * fix: add price and currency
    
    * fix: add productId
    
    * fix: append to wrapper, ITEM_ID
    
    * fix: remove comment ITEM_ID
    
    * fix: if price not exist
    
    * feat: add error if game not found

--- src/components/view/page/product-view/product-view.ts
+++ src/components/view/page/product-view/product-view.ts
@@ -8,6 +8,8 @@ import DefaultView from '../../default-view';
 import styleCss from './product-view.module.scss';
 import CreateTagElement from '../../../../utils/create-tag-element';
 
+const ERROR_NOT_FOUND_GAME = 'GAME not found';
+
 export default class ProductView extends DefaultView {
   private router: Router;
 
@@ -74,126 +76,140 @@ export default class ProductView extends DefaultView {
   }
 
   private renderProductCart(key: string) {
-    this.anonimApi.productProjectionResponseKEY(key).then((response) => {
-      console.log('product', response);
+    this.anonimApi
+      .productProjectionResponseKEY(key)
+      .then((response) => {
+        if (response.statusCode !== 200) {
+          this.createMessagePopup(ERROR_NOT_FOUND_GAME);
+        }
+        console.log('product', response);
 
-      const section = new ElementCreator({
-        tag: TagName.SECTION,
-        classNames: [styleCss['product-section']],
-        textContent: '',
+        const section = new ElementCreator({
+          tag: TagName.SECTION,
+          classNames: [styleCss['product-section']],
+          textContent: '',
+        });
+
+        const productImages = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-images']],
+          textContent: '',
+        });
+
+        const productImage = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-image']],
+          textContent: '',
+        });
+
+        if (response.body.masterVariant.images)
+          productImage.getElement().style.backgroundImage = `url(${response.body.masterVariant.images?.[0].url})`;
+        console.log(response.body.masterVariant.images?.[0].url);
+
+        const productInfo = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-info']],
+          textContent: 'PRODUCT INFO',
+        });
+
+        const productName = new ElementCreator({
+          tag: TagName.SPAN,
+          classNames: [styleCss['product-name']],
+          textContent: `${response.body.name.en}`,
+        });
+
+        const productCategory = new ElementCreator({
+          tag: TagName.SPAN,
+          classNames: [styleCss['product-category']],
+          // textContent: `CATEGORY: ${response.body.categories?.[0]}`,
+          textContent: `CATEGORY: ${response.body.masterVariant.attributes?.[3].value?.[0].key}`,
+        });
+
+        const price =
+          response.body.masterVariant.prices?.length !== 0
+            ? `PRICE: ${response.body.masterVariant.prices?.[0].value?.centAmount} ${response.body.masterVariant.prices?.[0].value?.currencyCode}`
+            : '';
+
+        const productPrice = new ElementCreator({
+          tag: TagName.SPAN,
+          classNames: [styleCss['product-price']],
+          textContent: price,
+        });
+
+        const productAttributes = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-attributes']],
+          textContent: `ATTRIBUTES:`,
+        });
+
+        const productDeveloper = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-developer'], styleCss.attributes],
+
+          textContent: `DEVELOPER: ${response.body.masterVariant.attributes?.[0].value[0]}`,
+        });
+
+        const productPlayersQuantity = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-developer'], styleCss.attributes],
+          textContent: `PLAYERS QUANTITY: ${response.body.masterVariant.attributes?.[1].value[0]}`,
+        });
+
+        const productPlatform = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-developer'], styleCss.attributes],
+          textContent: `PLATFORM: ${response.body.masterVariant.attributes?.[2].value[0].key}`,
+        });
+
+        const productGenre = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-developer'], styleCss.attributes],
+          textContent: `GENRE: ${response.body.masterVariant.attributes?.[3].value[0].key}`,
+        });
+
+        const productRelease = new ElementCreator({
+          tag: TagName.DIV,
+          classNames: [styleCss['product-developer'], styleCss.attributes],
+          textContent: `RELEASE: ${response.body.masterVariant.attributes?.[4].value[0]}`,
+        });
+
+        const productVideo = new CreateTagElement().createTagElement('span', [styleCss['product-video']], '');
+
+        // productVideo.setAttribute('data-href', `${response.body.masterVariant.attributes?.[5].value[0]}`);
+        // console.log(response.body.masterVariant.attributes?.[5].value[0]);
+
+        const productDescription = new ElementCreator({
+          tag: TagName.SPAN,
+          classNames: [styleCss['product-description']],
+          textContent: `DESCRIPTION: ${response.body.description?.en}`,
+        });
+
+        const addToCart = new ElementCreator({
+          tag: TagName.BUTTON,
+          classNames: [styleCss['product-description'], styleCss.button],
+          textContent: 'ADD TO CART',
+        });
+
+        productImages.addInnerElement(productImage);
+        section.addInnerElement(productImages);
+        section.addInnerElement(productInfo);
+        productInfo.addInnerElement(productName);
+        productInfo.addInnerElement(productCategory);
+        productInfo.addInnerElement(productPrice);
+        productInfo.addInnerElement(productDescription);
+        productInfo.addInnerElement(addToCart);
+        productInfo.addInnerElement(productAttributes);
+        productAttributes.addInnerElement(productDeveloper);
+        productAttributes.addInnerElement(productPlayersQuantity);
+        productAttributes.addInnerElement(productPlatform);
+        productAttributes.addInnerElement(productGenre);
+        productAttributes.addInnerElement(productRelease);
+        this.wrapper.append(section.getElement());
+        this.wrapper.append(productVideo);
+      })
+      .catch(() => {
+        this.createMessagePopup(ERROR_NOT_FOUND_GAME);
       });
-
-      const productImages = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-images']],
-        textContent: '',
-      });
-
-      const productImage = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-image']],
-        textContent: '',
-      });
-
-      if (response.body.masterVariant.images)
-        productImage.getElement().style.backgroundImage = `url(${response.body.masterVariant.images?.[0].url})`;
-      console.log(response.body.masterVariant.images?.[0].url);
-
-      const productInfo = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-info']],
-        textContent: 'PRODUCT INFO',
-      });
-
-      const productName = new ElementCreator({
-        tag: TagName.SPAN,
-        classNames: [styleCss['product-name']],
-        textContent: `${response.body.name.en}`,
-      });
-
-      const productCategory = new ElementCreator({
-        tag: TagName.SPAN,
-        classNames: [styleCss['product-category']],
-        // textContent: `CATEGORY: ${response.body.categories?.[0]}`,
-        textContent: `GENRE: ${response.body.masterVariant.attributes?.[3].value[0].key}`,
-      });
-
-      const productPrice = new ElementCreator({
-        tag: TagName.SPAN,
-        classNames: [styleCss['product-price']],
-        textContent: `PRICE: ${response.body.masterVariant.prices?.[0].value.centAmount} ${response.body.masterVariant.prices?.[0].value.currencyCode}`,
-      });
-
-      const productAttributes = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-attributes']],
-        textContent: `ATTRIBUTES:`,
-      });
-
-      const productDeveloper = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-developer'], styleCss.attributes],
-        textContent: `DEVELOPER: ${response.body.masterVariant.attributes?.[0].value[0]}`,
-      });
-
-      const productPlayersQuantity = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-developer'], styleCss.attributes],
-        textContent: `PLAYERS QUANTITY: ${response.body.masterVariant.attributes?.[1].value[0]}`,
-      });
-
-      const productPlatform = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-developer'], styleCss.attributes],
-        textContent: `PLATFORM: ${response.body.masterVariant.attributes?.[2].value[0].key}`,
-      });
-
-      const productGenre = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-developer'], styleCss.attributes],
-        textContent: `GENRE: ${response.body.masterVariant.attributes?.[3].value[0].key}`,
-      });
-
-      const productRelease = new ElementCreator({
-        tag: TagName.DIV,
-        classNames: [styleCss['product-developer'], styleCss.attributes],
-        textContent: `RELEASE: ${response.body.masterVariant.attributes?.[4].value[0]}`,
-      });
-
-      const productVideo = new CreateTagElement().createTagElement('span', [styleCss['product-video']], '');
-
-      // productVideo.setAttribute('data-href', `${response.body.masterVariant.attributes?.[5].value[0]}`);
-      // console.log(response.body.masterVariant.attributes?.[5].value[0]);
-
-      const productDescription = new ElementCreator({
-        tag: TagName.SPAN,
-        classNames: [styleCss['product-description']],
-        textContent: `DESCRIPTION: ${response.body.description?.en}`,
-      });
-
-      const addToCart = new ElementCreator({
-        tag: TagName.BUTTON,
-        classNames: [styleCss['product-description'], styleCss.button],
-        textContent: 'ADD TO CART',
-      });
-
-      productImages.addInnerElement(productImage);
-      section.addInnerElement(productImages);
-      section.addInnerElement(productInfo);
-      productInfo.addInnerElement(productName);
-      productInfo.addInnerElement(productCategory);
-      productInfo.addInnerElement(productPrice);
-      productInfo.addInnerElement(productDescription);
-      productInfo.addInnerElement(addToCart);
-      productInfo.addInnerElement(productAttributes);
-      productAttributes.addInnerElement(productDeveloper);
-      productAttributes.addInnerElement(productPlayersQuantity);
-      productAttributes.addInnerElement(productPlatform);
-      productAttributes.addInnerElement(productGenre);
-      productAttributes.addInnerElement(productRelease);
-      this.wrapper.append(section.getElement());
-      this.wrapper.append(productVideo);
-    });
   }
 
   private getProducts() {
@@ -215,7 +231,7 @@ export default class ProductView extends DefaultView {
   private createMessagePopup(message: string) {
     const messagePopup = new ElementCreator({
       tag: TagName.DIV,
-      classNames: [styleCss['login-form__popup']],
+      classNames: [styleCss['product-view__popup']],
       textContent: message,
     });
 
