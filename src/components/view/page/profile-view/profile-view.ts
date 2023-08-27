69233d9c0b97c9857057af0c2aefcf4aeb378d23
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-08-27 20:12:50 +0300

    feat: add functions for getting userData from server

--- src/components/view/page/profile-view/profile-view.ts
+++ src/components/view/page/profile-view/profile-view.ts
@@ -6,11 +6,16 @@ import Router from '../../../router/router';
 import LinkButton from '../../../shared/link-button/link-button';
 import DefaultView from '../../default-view';
 import styleCss from './profile-view.module.scss';
+import RegApi from '../../../../api/reg-api';
+import Address from './types/addresses/address';
+import UserData from './types/user-data';
 
 export default class ProfileView extends DefaultView {
   private router: Router;
 
   private wrapper: HTMLDivElement;
+
+  private userData: UserData;
 
   constructor(router: Router) {
     const params: ElementParams = {
@@ -26,7 +31,7 @@ export default class ProfileView extends DefaultView {
 
     this.getCreator().addInnerElement(this.wrapper);
 
-    this.configView();
+    this.userData = this.request();
   }
 
   public setContent(element: InsertableElement) {
@@ -40,6 +45,7 @@ export default class ProfileView extends DefaultView {
 
   private configView() {
     this.createContent();
+    console.log(this.userData);
   }
 
   private createContent() {
@@ -55,5 +61,91 @@ export default class ProfileView extends DefaultView {
       this.router.navigate(PagePath.INDEX);
     });
     return button;
+  }
+
+  private request() {
+    const api = new RegApi();
+    const userData: UserData = this.createUserData();
+    api.getCustomer('vladimir@sobbolev.ru').then((result) => {
+      const results = result.body.results[0];
+
+      if (results.firstName != null) {
+        userData.firstName = results.firstName;
+      }
+
+      if (results.lastName != null) {
+        userData.lastName = results.lastName;
+      }
+
+      if (results.dateOfBirth != null) {
+        userData.dateOfBirth = results.dateOfBirth;
+      }
+
+      userData.shippingAddresses = this.getShippingBillingAddresses(
+        results.addresses,
+        results.shippingAddressIds,
+        results.defaultShippingAddressId
+      );
+      userData.billingAddresses = this.getShippingBillingAddresses(
+        results.addresses,
+        results.billingAddressIds,
+        results.defaultBillingAddressId
+      );
+
+      this.configView();
+      return userData;
+    });
+    return userData;
+  }
+
+  private getShippingBillingAddresses(
+    allAddresses: Array<Address>,
+    addressesGroup: string[] | undefined,
+    defaultId: string | undefined
+  ) {
+    const addresses: Array<Address> = [];
+    console.log(allAddresses, 'all');
+    console.log(addressesGroup, 'group');
+    if (addressesGroup !== undefined) {
+      allAddresses.forEach((address) => {
+        if (addressesGroup.includes(address.id!)) {
+          if (address.id === defaultId) {
+            address.isDefault = true;
+          }
+          addresses.push(address);
+        }
+      });
+    }
+    console.log(addresses, 'aaaaa');
+    return addresses;
+  }
+
+  private createUserData(): UserData {
+    const message = 'unassigned';
+    return {
+      firstName: message,
+      lastName: message,
+      dateOfBirth: message,
+      shippingAddresses: [
+        {
+          city: message,
+          country: message,
+          id: message,
+          postalCode: message,
+          streetName: message,
+          isDefault: false,
+        },
+      ],
+      billingAddresses: [
+        {
+          city: message,
+          country: message,
+          id: message,
+          postalCode: message,
+          streetName: message,
+          isDefault: false,
+        },
+      ],
+    };
   }
 }
