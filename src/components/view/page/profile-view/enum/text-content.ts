d408e9088ae59107b1418df070b4cb1847058bb2
VladimirSobbolev <vladimir@sobbolev.ru>
2023-08-28 14:54:05 +0300

    feat: add text-content

---
+++ src/components/view/page/profile-view/enum/text-content.ts
@@ -0,0 +1,9 @@
+enum TextContent {
+  ADD_SHIPPING_ADDRESS_BUTTON = 'add shipping address',
+  ADD_BILLING_ADDRESS_BUTTON = 'add billing address',
+
+  REMOVE_SHIPPING_ADDRESS_BUTTON = 'remove shipping address',
+  REMOVE_BILLING_ADDRESS_BUTTON = 'remove billing address',
+}
+
+export default TextContent;
