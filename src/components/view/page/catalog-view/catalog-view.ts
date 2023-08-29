c587be8766858d52b16f6d4e6864964b2726a7d6
surface74 <warwhisper@tut.by>
2023-08-29 15:49:58 +0300

    feat: add class shared/ProductCard

--- src/components/view/page/catalog-view/catalog-view.ts
+++ src/components/view/page/catalog-view/catalog-view.ts
@@ -1,16 +1,22 @@
+import { Product } from '@commercetools/platform-sdk';
+import ProductApi from '../../../../api/products-api';
 import TagName from '../../../../enum/tag-name';
 import TagElement from '../../../../utils/create-tag-element';
 import ElementCreator, { ElementParams, InsertableElement } from '../../../../utils/element-creator';
-import { LinkName, PagePath } from '../../../router/pages';
+import { PagePath } from '../../../router/pages';
 import Router from '../../../router/router';
 import LinkButton from '../../../shared/link-button/link-button';
+// import ProductCard from '../../../shared/product-card/product-card';
 import DefaultView from '../../default-view';
 import styleCss from './catalog-view.module.scss';
+import ErrorMessage from '../../../message/error-message';
 
 export default class CatalogView extends DefaultView {
   private router: Router;
 
   private wrapper: HTMLDivElement;
+
+  private productApi = new ProductApi();
 
   constructor(router: Router) {
     const params: ElementParams = {
@@ -43,15 +49,28 @@ export default class CatalogView extends DefaultView {
   }
 
   private createContent() {
-    const button = this.createMainButton();
-
-    this.wrapper.append(button.getElement());
+    this.wrapper.replaceChildren('');
+    this.getProducts();
   }
 
-  private createMainButton() {
-    const button = new LinkButton(LinkName.INDEX, () => {
-      this.router.navigate(PagePath.INDEX);
+  private getProducts() {
+    this.productApi
+      .getProducts()
+      .then((response) => {
+        const products = response.body.results;
+        this.createProductLinks(products);
+      })
+      .catch((error) => new ErrorMessage().showMessage(error.message));
+  }
+
+  private createProductLinks(products: Product[]) {
+    products.forEach((product) => {
+      if (product.key) {
+        const link = new LinkButton(product.key, () => this.router.navigate(`${PagePath.CATALOG}/${product.key}`));
+        this.wrapper.append(link.getElement());
+
+        // const card = new ProductCard(product.key, this.router);
+      }
     });
-    return button;
   }
 }
