9f083e2ddb112af04c08eb4be2650ecd85c14899
VladimirSobbolev <vladimir@sobbolev.ru>
2023-09-01 11:42:07 +0300

    feat: add butons confirm and cancel

--- src/components/view/page/profile-view/user-field/password-changer.ts
+++ src/components/view/page/profile-view/user-field/password-changer.ts
@@ -3,11 +3,12 @@ import stylePasswordField from './styles/password-fields-style.module.scss';
 import stylePasswordWrap from './styles/password-wrap-style.module.scss';
 import TagName from '../../../../../enum/tag-name';
 import TextContent from '../enum/text-content';
-import { InputParams } from '../../../../../utils/input/inputParams';
+import { CallbackListener, InputParams } from '../../../../../utils/input/inputParams';
 import { InputTittles } from '../../../../../utils/input/input-values/input-values';
 import InputParamsCreator from '../../../../../utils/input/input-values/input-params-creator';
 import Events from '../../../../../enum/events';
 import InputCreator from '../../../../../utils/input/inputCreator';
+import buttonCreator from '../../../../shared/button/button-creator';
 
 export default class PasswordChanger {
   private passwordChangerElement: HTMLDivElement;
@@ -19,6 +20,10 @@ export default class PasswordChanger {
   private confirmOldPasswordField: InputCreator;
 
   private inputParamsCreator: InputParamsCreator;
+
+  private buttonConfirm: HTMLButtonElement;
+
+  private buttonCancel: HTMLButtonElement;
 
   constructor() {
     this.passwordChangerElement = this.createPasswordChangerElement();
@@ -39,6 +44,9 @@ export default class PasswordChanger {
       TextContent.OLD_PASSWORD_FIELD_LABEL
     );
 
+    this.buttonCancel = this.createButtonCancel();
+    this.buttonConfirm = this.createButtonConfirm();
+
     this.configureView();
   }
 
@@ -50,7 +58,9 @@ export default class PasswordChanger {
     wrap.append(
       this.newPasswordField.getElement(),
       this.repeatPasswordField.getElement(),
-      this.confirmOldPasswordField.getElement()
+      this.confirmOldPasswordField.getElement(),
+      this.buttonConfirm,
+      this.buttonCancel
     );
     this.passwordChangerElement.append(wrap);
   }
@@ -101,5 +111,26 @@ export default class PasswordChanger {
       checkingInput.setMessageError(InputTittles.PASSWORD_REPEAT);
       checkingInput.setCustomValidity(InputTittles.PASSWORD_REPEAT);
     }
+  }
+
+  private createButtonConfirm() {
+    return this.createButtons(TextContent.CONFIRM_BUTTON, this.confirmHandler.bind(this), Events.CLICK);
+  }
+
+  private createButtonCancel() {
+    return this.createButtons(TextContent.CANCEL_BUTTON, this.cancelHandler.bind(this), Events.CLICK);
+  }
+
+  private createButtons(textContent: string, eventListener?: CallbackListener, event?: string): HTMLButtonElement {
+    const button = new buttonCreator(textContent, undefined, eventListener, event);
+    return button.getButton();
+  }
+
+  private cancelHandler() {
+    console.log('cancel');
+  }
+
+  private confirmHandler() {
+    console.log('confirm');
   }
 }
