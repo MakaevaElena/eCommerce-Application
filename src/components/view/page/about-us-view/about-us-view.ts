650c8dcb244650b95debcb234769fd4f6fdc8fb1
VladimirSobbolev <vladimir@sobbolev.ru>
2023-09-11 15:42:36 +0300

    feat: add title

--- src/components/view/page/about-us-view/about-us-view.ts
+++ src/components/view/page/about-us-view/about-us-view.ts
@@ -1,11 +1,10 @@
 import TagName from '../../../../enum/tag-name';
 import TagElement from '../../../../utils/create-tag-element';
 import { ElementParams, InsertableElement } from '../../../../utils/element-creator';
-import { LinkName, PagePath } from '../../../router/pages';
 import Router from '../../../router/router';
-import LinkButton from '../../../shared/link-button/link-button';
 import DefaultView from '../../default-view';
-import styleCss from './about-us-view.module.scss';
+import styleCss from './styles/about-us-view.module.scss';
+import Title from './title/title';
 
 export default class AboutUsView extends DefaultView {
   private router: Router;
@@ -35,17 +34,20 @@ export default class AboutUsView extends DefaultView {
   private createLinks() {
     const wrapper = new TagElement().createTagElement('div', [styleCss['content-wrapper']]);
 
+    const title = new Title();
+
+    wrapper.append(title.getElement());
     this.getCreator().addInnerElement(wrapper);
   }
 
-  private createSignInButton() {
-    const linkButton = new LinkButton(LinkName.REGISTRATION, () => {
-      if (localStorage.getItem('isLogin') === 'true') {
-        this.router.navigate(PagePath.INDEX);
-      } else {
-        this.router.navigate(PagePath.REGISTRATION);
-      }
-    });
-    return linkButton;
-  }
+  // private createSignInButton() {
+  //   const linkButton = new LinkButton(LinkName.REGISTRATION, () => {
+  //     if (localStorage.getItem('isLogin') === 'true') {
+  //       this.router.navigate(PagePath.INDEX);
+  //     } else {
+  //       this.router.navigate(PagePath.REGISTRATION);
+  //     }
+  //   });
+  //   return linkButton;
+  // }
 }
---
+++ src/components/view/page/about-us-view/title/title.ts
@@ -0,0 +1,54 @@
+import styleTitle from './title-style.module.scss';
+import TagName from '../../../../../enum/tag-name';
+import tagName from '../../../../../enum/tag-name';
+
+export default class Title {
+  private element: HTMLDivElement;
+
+  constructor() {
+    this.element = this.createElement();
+    this.configure();
+  }
+
+  getElement() {
+    return this.element;
+  }
+
+  private createElement() {
+    const element = document.createElement(TagName.DIV);
+    element.classList.add(styleTitle.titleContainer);
+    return element;
+  }
+
+  private configure() {
+    this.element.append(this.createName(), this.createSubName());
+  }
+
+  private createName() {
+    const name = document.createElement(tagName.DIV);
+    name.classList.add(styleTitle.waving);
+    name.classList.add(styleTitle.title);
+    name.append(
+      this.addLetter('U'),
+      this.addLetter('P'),
+      this.addLetter('&'),
+      this.addLetter('G'),
+      this.addLetter('O')
+    );
+    return name;
+  }
+
+  private createSubName() {
+    const subName = document.createElement(tagName.DIV);
+    subName.classList.add(styleTitle.waving);
+    subName.classList.add(styleTitle.subtitle);
+    subName.append(this.addLetter('t'), this.addLetter('e'), this.addLetter('a'), this.addLetter('m'));
+    return subName;
+  }
+
+  private addLetter(letter: string) {
+    const letterElement = document.createElement(tagName.SPAN);
+    letterElement.innerText = letter;
+    return letterElement;
+  }
+}
