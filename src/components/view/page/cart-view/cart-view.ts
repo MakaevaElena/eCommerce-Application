5fbe0f6ab4c8c5e13c5c2691a813289b118ac44a
yamaslena@yandex.ru <yamaslena@yandex.ru>
2023-09-12 14:50:03 +0700

    feat: render total_cost and get value from api

--- src/components/view/page/cart-view/cart-view.ts
+++ src/components/view/page/cart-view/cart-view.ts
@@ -58,37 +58,44 @@ export default class CartView extends DefaultView {
             );
             if (item.productKey) this.cartItem.createCartItem(item.productKey, lineItemData);
           });
+
+          const totalPrice = `${(Number(cartResponse.body.totalPrice.centAmount) / 100).toFixed(2)} ${
+            cartResponse.body.totalPrice.currencyCode
+          }`;
+          this.createTotalOrderValue(totalPrice);
         })
         .catch((error) => new ErrorMessage().showMessage(error.message));
+
+    this.wrapper.append(this.cartSection.getElement());
   }
 
   private createCartHeader() {
     const header = new ElementCreator({
-      tag: TagName.SECTION,
+      tag: TagName.DIV,
       classNames: [styleCss['cart-header']],
       textContent: '',
     });
 
     const itemsImageTitle = new ElementCreator({
-      tag: TagName.SECTION,
+      tag: TagName.DIV,
       classNames: [styleCss['cart-header__image'], styleCss['cart-cell']],
       textContent: '',
     });
 
     const itemsTitle = new ElementCreator({
-      tag: TagName.SECTION,
+      tag: TagName.DIV,
       classNames: [styleCss['cart-header__title'], styleCss['cart-cell']],
       textContent: 'Items',
     });
 
     const itemQuantity = new ElementCreator({
-      tag: TagName.SECTION,
+      tag: TagName.DIV,
       classNames: [styleCss['cart-header__quantity'], styleCss['cart-cell']],
       textContent: 'Quantity',
     });
 
     const itemOrderValue = new ElementCreator({
-      tag: TagName.SECTION,
+      tag: TagName.DIV,
       classNames: [styleCss['cart-header__order-value'], styleCss['cart-cell']],
       textContent: 'Order Value',
     });
@@ -99,7 +106,31 @@ export default class CartView extends DefaultView {
     header.addInnerElement(itemOrderValue);
     this.cartSection.addInnerElement(header);
 
-    this.wrapper.append(this.cartSection.getElement());
+    // this.wrapper.append(this.cartSection.getElement());
+  }
+
+  private createTotalOrderValue(value: string) {
+    const totalCost = new ElementCreator({
+      tag: TagName.DIV,
+      classNames: [styleCss['cart-total-cost']],
+      textContent: '',
+    });
+
+    const orderTotalCostTitle = new ElementCreator({
+      tag: TagName.DIV,
+      classNames: [styleCss['cart-total-cost__title']],
+      textContent: 'TOTAL COST: ',
+    });
+
+    const orderTotalCost = new ElementCreator({
+      tag: TagName.DIV,
+      classNames: [styleCss['cart-total-cost__value']],
+      textContent: `${value}`,
+    });
+
+    totalCost.addInnerElement(orderTotalCostTitle);
+    totalCost.addInnerElement(orderTotalCost);
+    this.cartSection.addInnerElement(totalCost);
   }
 
   public setContent(element: InsertableElement) {
