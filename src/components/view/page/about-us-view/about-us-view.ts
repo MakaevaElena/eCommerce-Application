ba859f91f190fae72a7e03f820529861629bf838
Vladimir Sobolev <122258118+VladimirSobbolev@users.noreply.github.com>
2023-09-14 12:37:42 +0300

    About us/sprint 4 (#171)
    * feat: add title
    
    * feat: logo in progress
    
    * feat: add RSLOGO with link
    
    * refactor: add function createFunnyString in place of createName() and createSubName()
    
    * feat: create cards with img from repo
    
    * feat: create name in progress

--- src/components/view/page/about-us-view/about-us-view.ts
+++ src/components/view/page/about-us-view/about-us-view.ts
@@ -1,11 +1,13 @@
 import TagName from '../../../../enum/tag-name';
 import TagElement from '../../../../utils/create-tag-element';
 import { ElementParams, InsertableElement } from '../../../../utils/element-creator';
-import { LinkName, PagePath } from '../../../router/pages';
 import Router from '../../../router/router';
-import LinkButton from '../../../shared/link-button/link-button';
 import DefaultView from '../../default-view';
-import styleCss from './about-us-view.module.scss';
+import styleCss from './styles/about-us-view.module.scss';
+import Title from './title/title';
+import RSLogo from './RS_logo/RS-logo';
+import TeamMemberCard from './team-member-card/team-member-card';
+import TeamMemberCardType from './team-member-card/types/team-member-card-type';
 
 export default class AboutUsView extends DefaultView {
   private router: Router;
@@ -35,17 +37,53 @@ export default class AboutUsView extends DefaultView {
   private createLinks() {
     const wrapper = new TagElement().createTagElement('div', [styleCss['content-wrapper']]);
 
+    const logo = new RSLogo();
+    const title = new Title();
+    const elenaCard = new TeamMemberCard(this.createElenaData());
+    const siarheiCard = new TeamMemberCard(this.createSiarheiData());
+    const vladimirCard = new TeamMemberCard(this.createVladimirData());
+
+    wrapper.append(
+      logo.getElement(),
+      title.getElement(),
+      elenaCard.getElement(),
+      siarheiCard.getElement(),
+      vladimirCard.getElement()
+    );
     this.getCreator().addInnerElement(wrapper);
   }
 
-  private createSignInButton() {
-    const linkButton = new LinkButton(LinkName.REGISTRATION, () => {
-      if (localStorage.getItem('isLogin') === 'true') {
-        this.router.navigate(PagePath.INDEX);
-      } else {
-        this.router.navigate(PagePath.REGISTRATION);
-      }
-    });
-    return linkButton;
+  // private createSignInButton() {
+  //   const linkButton = new LinkButton(LinkName.REGISTRATION, () => {
+  //     if (localStorage.getItem('isLogin') === 'true') {
+  //       this.router.navigate(PagePath.INDEX);
+  //     } else {
+  //       this.router.navigate(PagePath.REGISTRATION);
+  //     }
+  //   });
+  //   return linkButton;
+  // }
+
+  //
+
+  private createSiarheiData(): TeamMemberCardType {
+    return {
+      photoSrc: 'https://avatars.githubusercontent.com/u/52540855?v=4',
+      name: 'Siarhei',
+    };
+  }
+
+  private createElenaData(): TeamMemberCardType {
+    return {
+      photoSrc: 'https://avatars.githubusercontent.com/u/87429007?v=4',
+      name: 'Makaeva Elena',
+    };
+  }
+
+  private createVladimirData(): TeamMemberCardType {
+    return {
+      photoSrc: 'https://avatars.githubusercontent.com/u/90175914?v=4',
+      name: 'Vladimir Sobolev',
+    };
   }
 }
