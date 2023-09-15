f48617a73123d533d38b2112576154e6e4dd2683
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-09-16 00:53:29 +0300

    feat: add intro

--- src/components/view/page/about-us-view/about-us-view.ts
+++ src/components/view/page/about-us-view/about-us-view.ts
@@ -53,10 +53,46 @@ export default class AboutUsView extends DefaultView {
     wrapper.append(
       logo.getElement(),
       title.getElement(),
+      this.createIntro(),
       elenaCard.getElement(),
       siarheiCard.getElement(),
       vladimirCard.getElement()
     );
     this.getCreator().addInnerElement(wrapper);
+  }
+
+  private createIntro(): HTMLDivElement {
+    const element = document.createElement(TagName.DIV);
+    element.classList.add(styleCss.intro);
+
+    const greeting = document.createElement('p');
+    greeting.classList.add(styleCss.intro__item);
+    greeting.textContent =
+      'Hi there! We are UP&GO team glad to see you in our app. The contribution of each of the participants in this work is huge. Some did more, some did less, but everyone tried to the best of their abilities. Not only mentally, but also in terms of willpower.';
+
+    const aboutRs = document.createElement('p');
+    aboutRs.classList.add(styleCss.intro__item);
+    aboutRs.textContent =
+      'To all who were able to do so, this is dedicated. To finish the course "JavaScript/Front-end" is not just to finish another course by defending a term paper or a diploma. It means to learn a lot of things, not only related to programming. It means learning to search for information, learning to express your thoughts, learning not only to ask for help, but also to help. This course taught us not just how to make beautiful websites, it taught us that programming is a huge community, which we became a part of thanks to Rolling Scopes.';
+
+    const gratitude = document.createElement('p');
+    gratitude.classList.add(styleCss.intro__item);
+    gratitude.textContent =
+      'We would like to say a huge thank you to our mentors and tutors for that. We are also grateful to our family and friends for their support. And we are especially grateful for the long nights, if they were shorter, we might not have made it. Special thanks to our mentor ';
+    const mentorLinkGitHub = document.createElement(TagName.A);
+    mentorLinkGitHub.classList.add(styleCss.link);
+    mentorLinkGitHub.href = 'https://github.com/mikaleinik';
+    mentorLinkGitHub.title = 'go to GitHub Mikhail Aleinik';
+    mentorLinkGitHub.target = 'blank';
+    mentorLinkGitHub.textContent = 'Mikhail Aleinik.';
+    gratitude.append(mentorLinkGitHub);
+
+    const summary = document.createElement('p');
+    summary.classList.add(styleCss.intro__item);
+    summary.textContent =
+      'Below you can familiarize yourself with each of the team members and their contribution to this project.';
+
+    element.append(greeting, aboutRs, gratitude, summary);
+    return element;
   }
 }
